dbb7c10fdca5ff64e9c54c4e281a6a1bc742cbf7
Abhinav12-tech <annuabhi1912@gmail.com>
2022-07-20 18:30:37 +0530

    First commit for Internship Project.

---
+++ src/main/java/com/abhinav/project/RestAPIProj/Controller/UserController.java
@@ -0,0 +1,52 @@
+package com.abhinav.project.RestAPIProj.Controller;
+
+import com.abhinav.project.RestAPIProj.Entity.User;
+import com.abhinav.project.RestAPIProj.Exception.ResourceNotFoundException;
+import com.abhinav.project.RestAPIProj.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return this.userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable(value = "id") long userId){
+        return this.userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found with id: "+ userId));
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user){
+        return this.userRepository.save(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@RequestBody User user, @PathVariable("id") long userId){
+        User existingUser = this.userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found with id: "+ userId));
+        existingUser.setName(user.getName());
+        existingUser.setPassword(user.getPassword());
+        this.userRepository.save(existingUser);
+        return this.userRepository.save(existingUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable("id") long userId){
+        User existingUser = this.userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found with id: "+ userId));
+        this.userRepository.delete(existingUser);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/com/abhinav/project/RestAPIProj/RestApiProjApplication.java
@@ -0,0 +1,13 @@
+package com.abhinav.project.RestAPIProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiProjApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RestApiProjApplication.class, args);
+	}
+}
